7deb4774aa2f178c580f452d36124708295e4372
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2025-07-09 11:20:27 +0900

    Noto Sans JPフォント追加

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'noto-sans-jp': ['"Noto Sans JP"', 'sans-serif'],
+        'raleway': ['Raleway', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
