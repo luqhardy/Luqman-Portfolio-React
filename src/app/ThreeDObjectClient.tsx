263bab3d95ba0b3a0fc15fc61b01a54aabb9882a
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2025-07-09 14:03:33 +0900

    Update ThreeDObjectClient.tsx

--- src/app/ThreeDObjectClient.tsx
+++ src/app/ThreeDObjectClient.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+//import React from "react";
 import dynamic from "next/dynamic";
 
 const ThreeDObject = dynamic(() => import("./ThreeDObject"), { ssr: false });
