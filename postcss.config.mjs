9c6cf1c472dfcba18a910ceba45fbb529e5882f5
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2025-07-09 10:38:29 +0900

    initial commit

---
+++ postcss.config.mjs
@@ -0,0 +1,12 @@
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+module.exports = {
+  theme: {
+    fontFamily: {
+	Raleway: ['Raleway', 'sans-serif'],
+    }
+  }
+}
+
+export default config;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="font-Inter text-center">
+  <h1>Luqman Hadi</h1>
+  </div>
+  );
+}
