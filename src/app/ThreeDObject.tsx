89757005fe10b98adc36f06a44630697aade4905
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2025-07-13 12:12:28 +0900

    3dモデル改善

--- src/app/ThreeDObject.tsx
+++ src/app/ThreeDObject.tsx
@@ -4,15 +4,15 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 
 function Model() {
   const gltf = useGLTF("/model.glb");
-  return <primitive object={gltf.scene} scale={2} />;
+  return <primitive object={gltf.scene} scale={1.5} />;
 }
 
 export default function ThreeDObject() {
   return (
     <div style={{ width: "300px", height: "200px", marginTop: "50px" }}>
-      <Canvas camera={{ position: [0, 0, 2], fov: 60 }}>
+      <Canvas camera={{ position: [0, 0, 2], fov: 50 }}>
         <ambientLight intensity={1} />
-        <directionalLight position={[2, 2, 2]} intensity={1} />
+        <directionalLight position={[2, 2, 2]} intensity={5} />
         <Model />
         <OrbitControls enableZoom={true} autoRotate={true} enableDamping={true} zoomToCursor={true} />
       </Canvas>
