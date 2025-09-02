9a90034177fdd02a0485c06d446aa55541fa0aed
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2025-09-02 11:54:26 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,12 +86,12 @@ function Home() {
   const content = {
     ja: {
       name: "ルクマン　ハディ",
-      subtitle: "文部科学省国費外国人留学生　神戸電子専門学校\nAIシステム開発学科　1年生\n マレーシア出身 神戸市在住\nメール: hello@luqmanhadi.com",
+      subtitle: "文部科学省国費外国人留学生　神戸電子専門学校\nAIシステム開発学科　1年生\n 株式会社ワオナス・一般社団法人クラウディス　パート社員\n　マレーシア出身 神戸市在住\nメール: hello@luqmanhadi.com",
       main: "Luqman Hadi"
     },
     en: {
       name: "ルクマン　ハディ",
-      subtitle: "Japanese Government (MEXT) Scholar, Kobe Institute of Computing\nAI Systems Development, 1st Year\n Malaysian, based in Kobe City, Hyogo Pref, Japan\nContact: hello@luqmanhadi.com",
+      subtitle: "Japanese Government (MEXT) Scholar, Kobe Institute of Computing\nAI Systems Development, 1st Year\n Wownas Co., Ltd. Freelance Staff\nMalaysian, based in Kobe City, Hyogo Pref, Japan\nContact: hello@luqmanhadi.com",
       main: "Luqman Hadi"
     }
   };
